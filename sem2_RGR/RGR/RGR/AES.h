668a16e9419916dbe97ef1f32ed5bdb921174882
aetoneilya <78798282+aetoneilya@users.noreply.github.com>
2021-09-06 11:56:38 +0700

    Delete sem2_RGR/RGR directory

--- sem2_RGR/RGR/RGR/AES.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector>
-#include <string>
-
-
-int aes_decode(std::string& out, std::vector<char> dataIn, char pass[]);
-int aes_encode(std::vector<char> dataIn, char pass[], std::string& out);
